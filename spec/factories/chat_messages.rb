58dd2bfb9285240120e350c075d1a1186c3991c6
Pawel Dedio <paweldedio@gmail.com>
2017-02-09 14:56:53 +0100

    Tests for chat_message model

--- spec/factories/chat_messages.rb
+++ spec/factories/chat_messages.rb
@@ -1,4 +1,7 @@
 FactoryGirl.define do
   factory :chat_message do
+    author_id {(create(:user)).id}
+    recipient_id {(create(:user)).id}
+    message {Faker::Lorem.sentence}
   end
 end
--- spec/models/chat_message_spec.rb
+++ spec/models/chat_message_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe ChatMessage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'chat_message should pass validation with correct data' do
+    message = build :chat_message
+    val = message.save
+
+    expect(val).to be true
+  end
+
+  describe 'validate author_id' do
+    it 'should validate presence of' do
+      should validate_presence_of(:author_id)
+    end
+  end
+
+  describe 'validate recipient_id' do
+    it 'should validate presence of' do
+      should validate_presence_of(:recipient_id)
+    end
+
+    it 'should not allow to add the same recipient as author' do
+      message = build :chat_message
+      message.recipient_id = message.author_id
+      val = message.save
+
+      expect(val).to be false
+    end
+
+    it 'should not allow to add recipient which is not a friend' do
+      message = build :chat_message
+      message.recipient_id = (create :user).id
+      val = message.save
+
+      expect(val)
+    end
+  end
+
+  describe 'validate message' do
+    it 'should validate presence of' do
+      should validate_presence_of(:message)
+    end
+  end
 end
