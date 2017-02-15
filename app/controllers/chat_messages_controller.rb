025996dcf49e6239ca407132b356093aee039f60
Pawel Dedio <paweldedio@gmail.com>
2017-02-15 14:36:14 +0100

    Pagination fix

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -6,8 +6,9 @@ class ChatMessagesController < ApplicationController
     @chat_messages = ChatMessage.unread_thread current_user, params[:participant]
 
     setup_pagination(page: params[:page], page_size: params[:page_size])
-    setup_sorting(sort_by: params[:sort_by], sort_order: params[:sort_order])
-    collection = search_collection(@chat_messages, search_field: params[:search_field], search_value: params[:search_value])
+    setup_sorting(sort_by: params[:sort_by], sort_order: params[:sort_order], model: ChatMessage)
+    collection = @chat_messages.page(@page).per(@per_page)
+    collection = search_collection(collection, search_field: params[:search_field], search_value: params[:search_value])
     render json: collection,
            serializer: CollectionSerializer,
            each_serializer: ChatMessageSerializer,
